cc755f22208b6fcc2440e65d5625b197e6e24bcc
Leo Soares <leonardo.soares@mollie.com>
2020-02-01 15:11:55 +0100

    Quote URL has changed since implementation and stocks can be non integer values

--- quote.go
+++ quote.go
@@ -2,9 +2,9 @@ package finnhub
 
 // Quote the data structure for quotes
 type Quote struct {
-	Open          int `json:"o"`
-	High          int `json:"h"`
-	Low           int `json:"l"`
-	Current       int `json:"c"`
-	PreviousClose int `json:"pc"`
+	Open          float64 `json:"o"`
+	High          float64 `json:"h"`
+	Low           float64 `json:"l"`
+	Current       float64 `json:"c"`
+	PreviousClose float64 `json:"pc"`
 }
